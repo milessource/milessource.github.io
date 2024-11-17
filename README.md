29aa9c557d56630eba70a0a685dd4b5b51b5affa
Mak Moskalenko <risknu@ukr.net>
2024-11-17 14:34:13 +0100

    Small bug fixes(copy fix)
    Fixed small bugs(copy bug) also fixed icons etc.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 A simple site with some fun stuff, seems to work, turned out awesome.  
 
-### `functions.js`  
+### functions.js
 For editing what will be copied when a person clicks the "Copy IP" button:  
 ```js
 const IP_TO_COPY = "x.y.z.w:xxxx";
@@ -11,7 +11,7 @@ const IP_TO_COPY = "x.y.z.w:xxxx";
 
 **DEBUG NOTE**  
 ```js
-// id's -> download_modpack | copy_ip | join_discord
+// id's -> download_modpack | copy_ip | join_discord | ip_field
 ```
 
 ## License
--- functions.js
+++ functions.js
@@ -1,12 +1,16 @@
-// id's -> download_modpack | copy_ip | join_discord
+// id's -> download_modpack | copy_ip | join_discord | ip_field
 
 const IP_TO_COPY = "x.y.z.w:xxxx";
+
+document.getElementById('ip_field').value = IP_TO_COPY;
 
 document.getElementById('join_discord').addEventListener('click', () => {
     window.location.replace('http://miless.duckdns.org/discord');
 });
 
 document.getElementById('copy_ip').addEventListener('click', async () => {
-    try { await navigator.clipboard.writeText(IP_TO_COPY); } 
-    catch (err) { console.error('Error copy to clipboard -> ', err); }
+    const ip_field = document.getElementById('ip_field');
+    ip_field.focus(); ip_field.select();
+    try { document.execCommand('copy'); } 
+    catch (err) {console.error('Unable to copy to clipboard -> ', err); }
 });
